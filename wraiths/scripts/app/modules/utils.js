4eae20e14715385f5c59c8f0c3fea49a661140a8
kreo <hi@kreo.me>
2016-08-26 17:01:13 +0200

    remove load_script

--- wraiths/scripts/app/modules/utils.js
+++ wraiths/scripts/app/modules/utils.js
@@ -97,26 +97,6 @@ var wraith_utils = (function () {
 				this.classList.toggle('active');
 			});
 
-		projectName.globals.load_scripts = function (array, callback) {
-			var loader = function (src, handler) {
-				var script = document.createElement("script");
-				script.src = src;
-				script.onload = script.onreadystatechange = function () {
-					script.onreadystatechange = script.onload = null;
-					handler();
-				};
-				var head = document.getElementsByTagName("head")[0];
-				(head || document.body).appendChild(script);
-			};
-			(function () {
-				if (array.length !== 0) {
-					loader(('https:' === location.protocol ? 'https:' : 'http:') + array.shift(), arguments.callee);
-				} else {
-					callback && callback();
-				}
-			})();
-		};
-
 		projectName.globals.get_filename = function (path) {
 			return path.split('/').reverse()[0].replace(/\.[^/.]+$/, '');
 		};
