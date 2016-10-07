0761f3181a5f7371afb74d34189299d2048aecd9
Sergio Pellicani <sergio@preventivi.it>
2016-10-07 13:09:23 +0200

    Bug fix su nav-bar mobile quando si apre la nav-bar con la gesture. Se il menu è vuoto adesso la nav-bar non si apre.

--- sources/scripts/modules/nav-mobile.js
+++ sources/scripts/modules/nav-mobile.js
@@ -4,59 +4,69 @@
 
 module.exports = (function () {
 
-	// Private Methods
-	// ---------------------------------------------
+    // Private Methods
+    // ---------------------------------------------
 
-	function nav_open(button, overlay) {
-		$(overlay + ',' + button).addClass('s--active');
-	}
+    function nav_open(button, overlay) {
+        $(overlay + ',' + button).addClass('s--active');
+    }
 
-	function nav_close(button, overlay) {
-		$(overlay + ',' + button).removeClass('s--active');
-	}
+    function nav_close(button, overlay) {
+        $(overlay + ',' + button).removeClass('s--active');
+    }
 
-	function nav_toggle(slideout, button, overlay) {
+    function nav_toggle(slideout, button, overlay, menu) {
 
-		slideout.on('open', function () {
-			nav_open(button, overlay);
-		});
+        if ($('#' + menu + ' li').length > 0) {
+            slideout.on('open', function () {
+                nav_open(button, overlay);
+            });
 
-		slideout.on('beforeopen', function () {
-			nav_open(button, overlay);
-		});
+            slideout.on('beforeopen', function () {
+                nav_open(button, overlay);
+            });
 
-		slideout.on('close', function () {
-			nav_close(button, overlay);
-		});
+            slideout.on('close', function () {
+                nav_close(button, overlay);
+            });
 
-		slideout.on('beforeclose', function () {
-			nav_close(button, overlay);
-		});
-	}
+            slideout.on('beforeclose', function () {
+                nav_close(button, overlay);
+            });
+        } else {
+            slideout.destroy();
+        }
 
-	// Public Methods
-	// ---------------------------------------------
+    }
 
-	function slide_out(button, main, menu, overlay) {
+    // Public Methods
+    // ---------------------------------------------
 
-		var slideout = new Slideout({
-			'panel': document.getElementById(main),
-			'menu': document.getElementById(menu),
-			'padding': 256,
-			'tolerance': 70
-		});
+    function slide_out(button, main, menu, overlay) {
 
-		$(button + ',' + overlay).on('click', function () {
-			slideout.toggle();
-			nav_toggle(slideout, button, overlay);
-		});
-	}
+        var slideout = new Slideout({
+            'panel': document.getElementById(main),
+            'menu': document.getElementById(menu),
+            'padding': 256,
+            'tolerance': 70
+        });
 
-	// Module API
-	// ---------------------------------------------
+        $(button + ',' + overlay).on('click', function () {
+            slideout.toggle();
+            nav_toggle(slideout, button, overlay, menu);
+        });
 
-	return {
-		slide_out: slide_out
-	};
+        slideout.on('translate', function () {
+            nav_toggle(slideout, button, overlay, menu);
+        });
+
+    }
+
+    // Module API
+    // ---------------------------------------------
+
+    return {
+        slide_out: slide_out
+    };
 
 })();
--- sources/scripts/modules/utils.js
+++ sources/scripts/modules/utils.js
@@ -4,120 +4,120 @@
 
 module.exports = (function () {
 
-	// Private Methods
-	// ---------------------------------------------
+    // Private Methods
+    // ---------------------------------------------
 
-	// Public Methods
-	// ---------------------------------------------
+    // Public Methods
+    // ---------------------------------------------
 
-	function init(projectName) {
+    function init(projectName) {
 
-		$.fn.ajax_submit_form = function (url, options) {
+        $.fn.ajax_submit_form = function (url, options) {
 
-			if (options === undefined) {
-				options = {};
-			}
+            if (options === undefined) {
+                options = {};
+            }
 
-			var data = $(this).serialize();
+            var data = $(this).serialize();
 
-			var ajax_options = {
-				type: "POST",
-				url: url,
-				data: data,
-				dataType: 'json'
-			};
+            var ajax_options = {
+                type: "POST",
+                url: url,
+                data: data,
+                dataType: 'json'
+            };
 
-			var jqXHR = $.ajax($.extend(ajax_options, options));
+            var jqXHR = $.ajax($.extend(ajax_options, options));
 
-			if (options.done_fun !== undefined) {
-				jqXHR.done(options.done_fun);
-			}
+            if (options.done_fun !== undefined) {
+                jqXHR.done(options.done_fun);
+            }
 
-			if (options.fail_fun !== undefined) {
-				jqXHR.fail(options.fail_fun);
-			}
-		};
+            if (options.fail_fun !== undefined) {
+                jqXHR.fail(options.fail_fun);
+            }
+        };
 
-		$.fn.exists = function () {
-			return !!this.length;
-		};
+        $.fn.exists = function () {
+            return !!this.length;
+        };
 
-		$.fn.display_block = function () {
+        $.fn.display_block = function () {
 
-			this.removeClass('hide').addClass('show');
-		};
+            this.removeClass('hide').addClass('show');
+        };
 
-		$.fn.display_none = function () {
+        $.fn.display_none = function () {
 
-			this.removeClass('show').addClass('hide');
-		};
+            this.removeClass('show').addClass('hide');
+        };
 
-		$.fn.handle_content_height = function (options) {
+        $.fn.handle_content_height = function (options) {
 
-			var params = options || {};
+            var params = options || {};
 
-			var window_h = $(window).height();
-			var main_h = $('main').innerHeight();
-			var content_h = $('.content').innerHeight();
-			var footer_h = $('.footer').innerHeight();
+            var window_h = $(window).height();
+            var main_h = $('main').innerHeight();
+            var content_h = $('.content').innerHeight();
+            var footer_h = $('.footer').innerHeight();
 
-			if (main_h - footer_h <= (window_h - (main_h - content_h))) {
-				$('.content').css({
-					height: (window_h - (main_h - content_h)) + 'px'
-				});
-			}
-		};
+            if (main_h - footer_h <= (window_h - (main_h - content_h))) {
+                $('.content').css({
+                    height: (window_h - (main_h - content_h)) + 'px'
+                });
+            }
+        };
 
-		$.fn.remove_class_except = function (val) {
-			return this.each(function (index, el) {
-				var keep = val.split(" "), // list we'd like to keep
-					reAdd = [], // ones that should be re-added if found
-					$el = $(el); // element we're working on
-				// look for which we re-add (based on them already existing)
-				for (var c = 0; c < keep.length; c++) {
-					if ($el.hasClass(keep[c])) reAdd.push(keep[c]);
-				}
+        $.fn.remove_class_except = function (val) {
+            return this.each(function (index, el) {
+                var keep = val.split(" "), // list we'd like to keep
+                    reAdd = [], // ones that should be re-added if found
+                    $el = $(el); // element we're working on
+                // look for which we re-add (based on them already existing)
+                for (var c = 0; c < keep.length; c++) {
+                    if ($el.hasClass(keep[c])) reAdd.push(keep[c]);
+                }
 
-				// drop all, and only add those confirmed as existing
-				$el.removeClass() // remove existing classes
-					.addClass(reAdd.join(' ')); // re-add the confirmed ones
-			});
-		};
+                // drop all, and only add those confirmed as existing
+                $el.removeClass() // remove existing classes
+                    .addClass(reAdd.join(' ')); // re-add the confirmed ones
+            });
+        };
 
-		$.fn.fill_nav = function (li, dest) {
-			$(li).each(function () {
-				var cp = $(this).clone();
-				cp.remove_class_except('s--active').removeAttr('id');
-				cp.appendTo($(dest));
-			});
-		};
+        $.fn.fill_nav = function (li, dest) {
+            $(li).each(function () {
+                var cp = $(this).clone();
+                cp.remove_class_except('s--active').removeAttr('id');
+                cp.appendTo($(dest));
+            });
+        };
 
-		document.querySelector('.navbar__toggle')
-			.addEventListener('click', function () {
-				this.classList.toggle('active');
-			});
+        document.querySelector('.navbar__toggle')
+            .addEventListener('click', function () {
+                this.classList.toggle('active');
+            });
 
-		projectName.globals.get_filename = function (path) {
-			return path.split('/').reverse()[0].replace(/\.[^/.]+$/, '');
-		};
+        projectName.globals.get_filename = function (path) {
+            return path.split('/').reverse()[0].replace(/\.[^/.]+$/, '');
+        };
 
-		projectName.globals.object_chain = function (chains) {
-			var c = Object.prototype;
+        projectName.globals.object_chain = function (chains) {
+            var c = Object.prototype;
 
-			while (chains.length) {
-				c = Object.create(c);
-				$.extend(c, chains.pop()); // some function that does mixin
-			}
+            while (chains.length) {
+                c = Object.create(c);
+                $.extend(c, chains.pop()); // some function that does mixin
+            }
 
-			return c;
-		};
-	}
+            return c;
+        };
+    }
 
-	// Module API
-	// ---------------------------------------------
+    // Module API
+    // ---------------------------------------------
 
-	return {
-		init: init
-	};
+    return {
+        init: init
+    };
 
 })();
